a0a8ad0c0a510eae9dd542bd7e7f426a3fa2fcb3
Nomer77 <golyshev7707@icloud.com>
2022-09-06 23:26:19 +0000

    Version 1.11 Sorted task list

--- handlers/task_operation/open_task_opera.py
+++ handlers/task_operation/open_task_opera.py
@@ -8,9 +8,19 @@ async def open_tasks(message: types.Message):
     if bool(await data.get_tasks(tele_id=message.from_user.id)):
         await bot.send_message(message.from_user.id, "Вот ваши задачи", reply_markup=markup.task_button())
         amount = 0
+        noncom_task = {}
         for value in await data.get_tasks(tele_id=message.from_user.id, is_complete=0):
+            noncom_task[value[0]] = await data.get_task_time(message.from_user.id, value[0])
+        for k, v in noncom_task.items():
+            if list(v[0])[0] is None:
+                noncom_task[k] = -1
+            else:
+                noncom_task[k] = list(v[0])[0]
+        print(noncom_task)
+        sorted_task = dict(sorted(noncom_task.items(), key=lambda x: x[1]))
+        for value in sorted_task:
             amount += 1
-            await bot.send_message(message.from_user.id, f"{amount}. {value[0]}")
+            await bot.send_message(message.from_user.id, f"{amount}. {value}")
         for value in await data.get_tasks(tele_id=message.from_user.id, is_complete=1):
             amount += 1
             await bot.send_message(message.from_user.id, f"<s>{amount}. <i>{value[0]}</i></s>", parse_mode='html')
--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-import math
-print(math.inf)
+a = {1: 0, 2: 0, 3: 0}
+for k, v in a.items():
+    print(f"k = {k}, v = {v}")
+    if v == 0:
+        a[k] = None
+
+print(a)
