04eb706a064df12d2faa2a08b37d2c0b0871861b
Nomer77 <golyshev7707@icloud.com>
2022-09-11 17:11:30 +0000

    Version 1.43 Fixed time

--- handlers/commands.py
+++ handlers/commands.py
@@ -3,6 +3,7 @@ from aiogram.dispatcher import Dispatcher
 from config_bot import data
 import markup
 from loguru import logger
+from handlers.task_operation.open_task_opera import open_tasks
 
 
 # @dp.message_handler(commands=['start'])
@@ -17,9 +18,29 @@ async def command_start(message: types.Message):
 async def command_menu(message: types.Message):
     await message.answer("Уже открыл!", reply_markup=markup.menu_buttons())
     logger.info(f"User ({message.from_user.id}) {message.from_user.username} opened menu")
+    if not await data.is_user_exists(message.from_user.id):
+        await data.add_user(message.from_user.id)
+        logger.info(f"Bot created new user ({message.from_user.id})")
+
+
+async def command_task_list(message: types.Message):
+    await open_tasks(message)
+    logger.info(f"User ({message.from_user.id}) {message.from_user.username} took task list")
+
+
+async def command_help(message: types.Message):
+    await message.answer("Это бот создан быть менеджером ваших задач. Вы можете выйти в главное меню командой /menu")
+    await message.answer("Вы можете получить список ваших задач командой /tasklist. Перейдя в раздел задачи из "
+                         "главного меню, вы сможете взаимодействовать с этим списком: добавлять, удалять, выполнять и "
+                         "редактировать задачи")
+    if not await data.is_user_exists(message.from_user.id):
+        await data.add_user(message.from_user.id)
+        logger.info(f"Bot created new user ({message.from_user.id})")
 
 
 def register_handlers_commands(dp: Dispatcher):
+    dp.register_message_handler(command_help, commands=['help'])
     dp.register_message_handler(command_start, commands=['start'])
     dp.register_message_handler(command_menu, commands=['menu'])
+    dp.register_message_handler(command_task_list, commands=['tasklist'])
     dp.register_message_handler(command_menu, lambda message: message.text == "Меню")
--- time_manager.py
+++ time_manager.py
@@ -9,17 +9,20 @@ def find_time(task):
     text = task.strip()
     nums = '1234567890'
     pos = text.find(':', len(text) - 5, len(text) - 1)
-    if (pos != -1) and (text[pos - 1] in nums) and (text[pos + 1] in nums):
-        try:
-            a = int(text[pos - 2].strip())
-        except ValueError:
-            a = 0
-        b = int(text[pos - 1])
-        c = int(text[pos + 1])
-        d = int(text[pos + 2])
-        time = (a * 10 + b) * 3600 + (c * 10 + d) * 60
-        if time > (now_sec_time()):
-            return time
+    try:
+        if (pos != -1) and (text[pos - 1] in nums) and (text[pos + 1] in nums) and (text[pos + 2] in nums):
+            try:
+                a = int(text[pos - 2].strip())
+            except ValueError:
+                a = 0
+            b = int(text[pos - 1])
+            c = int(text[pos + 1])
+            d = int(text[pos + 2])
+            time = (a * 10 + b) * 3600 + (c * 10 + d) * 60
+            if time > (now_sec_time()):
+                return time
+    except IndexError:
+        pass
     return None
 
 
